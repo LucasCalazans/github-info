95efb4b423e7c7229bfd1bc5a02a1e23620bcf3c
Lucas Calazans <calazans95@hotmail.com>
2018-06-25 07:10:33 +0000

    #10 Adding loader to repolist

--- src/components/RepoList/view.jsx
+++ src/components/RepoList/view.jsx
@@ -1,10 +1,14 @@
 import { h } from 'preact';
 import RepoItem from '../RepoItem';
+import { Loader } from '../Base';
 
 const RepoListView = props => (
-    <ul className="repo-list">
-        <RepoItem name="Repository" description="Description" license="License" stars={ 12000 } />
-    </ul>
+    <div className="repo-list-container">
+        <ul className="repo-list">
+            <RepoItem name="Repository" description="Description" license="License" stars={ 12000 } />
+        </ul>
+        <Loader />
+    </div>
 );
 
 export default RepoListView;
